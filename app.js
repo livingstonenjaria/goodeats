15c501e7829cc907897bb97b8bb5acc27b1cafb5
Livingstone Wachira <livingstonenjaria4@gmail.com>
2020-07-05 10:02:02 +0300

    Created a general 404 error middleware in app.js

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 // * Third Party Libraries
 const express = require('express');
 
+// * Custom file imports
+
 // * Initiallizations
 const app = express();
 const port = 3000;
 
+// * Filtering Routes
 
+// * General 404 error
+app.use((req, res, next) => {
+    res.status(404).json({
+        errorCode: 404,
+        message: "Sorry that route does not exist"
+    });
+});
 app.listen(port, () => console.log(`Server listening on port ${port}`));
