a19a115f992ffeade50bfc782244cf38f1873ea8
davidmutonga <nedelmutonga@gmail.com>
2020-07-23 11:20:17 +0300

    Added admin controller

---
+++ api/v1/controllers/admincontroller.js
@@ -0,0 +1,12 @@
+// * Third party libs
+const mongoose = require('mongoose')
+const createError = require('http-errors')
+const { validationResult } = require('express-validator')
+
+// * Custom file imports
+// * @desc models imports
+const Country = require('../models/country')
+
+module.exports = {
+  addCountry: async (req, res, next) => {},
+}
